ade5a95195f086905b77361d0a4ab07a7e1f9f5d
frotveit <frotveit@outlook.com>
2023-01-06 10:50:02 +0100

    cleanup

--- AdventOfCode/Helpers/FileUtil.cs
+++ AdventOfCode/Helpers/FileUtil.cs
@@ -2,16 +2,17 @@
 
 namespace AdventOfCode.Helpers
 {
-    public class FileUtil
+    public static class FileUtil
     {
-        public static string[] Get2017File(string filename)
+        public static string[] GetFile(string relLocation, string filename)
         {
-            string[] lines = File.ReadAllLines(@"../../2017/Data/" + filename);
+            string[] lines = File.ReadAllLines($"../../../{relLocation}/{filename}");
             return lines;
         }
-        public static string[] Get2018File(string filename)
+
+        public static string[] Get2017File(string filename)
         {
-            string[] lines = File.ReadAllLines(@"../../2018/Data/" + filename);
+            string[] lines = File.ReadAllLines(@"../../../2017/Data/" + filename);
             return lines;
         }
 
@@ -21,5 +22,18 @@ namespace AdventOfCode.Helpers
             if (lines.Length > 0) return lines[0];
             return "";
         }
+
+        public static string[] Get2018File(string filename)
+        {
+            string[] lines = File.ReadAllLines(@"../../../2018/Data/" + filename);
+            return lines;
+        }
+
+        public static string Get2018FileLine(string filename)
+        {
+            string[] lines = Get2018File(filename);
+            if (lines.Length > 0) return lines[0];
+            return "";
+        }        
     }
 }
--- AdventOfCode/Test/TestFSharp.cs
+++ AdventOfCode/Test/TestFSharp.cs
@@ -1,6 +1,4 @@
-﻿
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCode.Test
 {
